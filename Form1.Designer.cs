6ac7faba47f4dfc6b96d6c51582c4ac558cba1af
mazhar1341 <faizimazhar2@gmail.com>
2025-03-10 14:01:44 +1300

    Mazhar: Added the bew checkbox.

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -30,31 +30,46 @@
         {
             button1 = new Button();
             textBox1 = new TextBox();
+            checkBox1 = new CheckBox();
             SuspendLayout();
             // 
             // button1
             // 
-            button1.Location = new Point(153, 111);
+            button1.Location = new Point(134, 83);
+            button1.Margin = new Padding(3, 2, 3, 2);
             button1.Name = "button1";
-            button1.Size = new Size(108, 29);
+            button1.Size = new Size(94, 22);
             button1.TabIndex = 0;
             button1.Text = "Display Text ";
             button1.UseVisualStyleBackColor = true;
             // 
             // textBox1
             // 
-            textBox1.Location = new Point(322, 113);
+            textBox1.Location = new Point(282, 85);
+            textBox1.Margin = new Padding(3, 2, 3, 2);
             textBox1.Name = "textBox1";
-            textBox1.Size = new Size(252, 27);
+            textBox1.Size = new Size(221, 23);
             textBox1.TabIndex = 1;
+            // 
+            // checkBox1
+            // 
+            checkBox1.AutoSize = true;
+            checkBox1.Location = new Point(392, 185);
+            checkBox1.Name = "checkBox1";
+            checkBox1.Size = new Size(82, 19);
+            checkBox1.TabIndex = 2;
+            checkBox1.Text = "checkBox1";
+            checkBox1.UseVisualStyleBackColor = true;
             // 
             // Form1
             // 
-            AutoScaleDimensions = new SizeF(8F, 20F);
+            AutoScaleDimensions = new SizeF(7F, 15F);
             AutoScaleMode = AutoScaleMode.Font;
-            ClientSize = new Size(800, 450);
+            ClientSize = new Size(700, 338);
+            Controls.Add(checkBox1);
             Controls.Add(textBox1);
             Controls.Add(button1);
+            Margin = new Padding(3, 2, 3, 2);
             Name = "Form1";
             Text = "Form1";
             ResumeLayout(false);
@@ -65,5 +80,6 @@
 
         private Button button1;
         private TextBox textBox1;
+        private CheckBox checkBox1;
     }
 }
